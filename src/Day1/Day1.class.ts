540d83f0e59bf7c199e99b5a95ba3daf5650139f
German Guerrero <german@ilcolombia.com>
2025-12-14 18:44:34 -0500

    ✨Core day initializaion done

--- src/Day1/Day1.class.ts
+++ src/Day1/Day1.class.ts
@@ -2,11 +2,10 @@ import { DayProblem } from "../DayProblem.interface";
 
 export class Day1 implements DayProblem {
     day: number = 1;
-    part1(input: string) {
+    part1(input: string): Promise<string> {
+        return Promise.resolve("Hello World");
+    }
+    part2(input: string): Promise<string> {
         throw new Error("Method not implemented.");
     }
-    part2(input: string) {
-        throw new Error("Method not implemented.");
-    }
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { Day1 } from "./Day1/Day1.class";
 import { DayProblem } from "./DayProblem.interface";
+import * as fs from "fs";
 
-const days: DayProblem[] = [new Day1()]
+const days: DayProblem[] = [new Day1()];
 
 const options = {
     day: 1,
     part: 1,
     isTest: true,
-}
+};
 
-const fileUrl = `files/Day${options.day}/.`
+const fileUrl = `files/Day${options.day}/${options.isTest ? "test" : "end"}-input.txt`;
+
+const input = fs.readFileSync(fileUrl, "utf-8");
+
+const day: DayProblem = days[options.day - 1];
+
+let response: Promise<string>;
+
+if (options.part == 1) response = day.part1(input);
+else response = day.part2(input);
+
+response.then((response) => {
+    const writeUrl = `files/Day${options.day}/${options.isTest ? "test" : "end"}-response.txt`;
+    fs.writeFileSync(writeUrl, response, { encoding: "utf-8" });
+    console.log(response);
+});
